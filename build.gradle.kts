1184554ba20ac5a409526bb68754da861c11e9f9
furukawa1020 <f.kotaro.0530@gmail.com>
2026-02-03 16:17:27 +0900

    chore: Add generated Android debug build intermediate files and merged resources.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("javax.xml.bind:jaxb-api:2.3.1")
+        classpath("org.glassfish.jaxb:jaxb-runtime:2.3.1")
+    }
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.5.1" apply false
