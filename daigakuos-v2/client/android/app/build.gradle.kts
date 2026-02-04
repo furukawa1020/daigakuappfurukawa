fd02c1032f52e23ce93c513e6932c247d54b35c9
furukawa1020 <f.kotaro.0530@gmail.com>
2026-02-04 17:00:35 +0900

    feat: Add initial Flutter client project configuration files including dependencies and Android build setup.

--- daigakuos-v2/client/android/app/build.gradle.kts
+++ daigakuos-v2/client/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.daigaku_os_client"
+    namespace = "com.example.daigaku_app_client"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.daigaku_os_client"
+        applicationId = "com.example.daigaku_app_client"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
