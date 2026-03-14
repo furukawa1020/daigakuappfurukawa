013c25ab2722de3c9d702beafdf833ad5f2ff198
furukawa1020 <f.kotaro.0530@gmail.com>
2026-03-14 15:59:33 +0900

    feat: Add a new HyperfocusButton widget with hold-to-complete functionality, animated progress, and haptic feedback.

--- daigakuos-v2/client/lib/widgets/hyperfocus_button.dart
+++ daigakuos-v2/client/lib/widgets/hyperfocus_button.dart
@@ -16,6 +16,7 @@ class HyperfocusButton extends ConsumerStatefulWidget {
   ConsumerState<HyperfocusButton> createState() => _HyperfocusButtonState();
 }
 
+class _HyperfocusButtonState extends ConsumerState<HyperfocusButton> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _scaleAnimation;
 
