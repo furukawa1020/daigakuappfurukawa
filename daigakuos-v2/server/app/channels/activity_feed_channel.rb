ea7b1c3dacd1fc5a42f0e8fd4dd4338ae4dc6dca
furukawa1020 <f.kotaro.0530@gmail.com>
2026-03-27 14:00:21 +0900

    　　　

--- daigakuos-v2/server/app/channels/activity_feed_channel.rb
+++ daigakuos-v2/server/app/channels/activity_feed_channel.rb
@@ -3,13 +3,12 @@ class ActivityFeedChannel < ApplicationCable::Channel
     stream_from "activity_feed"
   end
 
-  def self.broadcast_activity(user, type, metadata)
+  def self.broadcast_moko_party(users)
     ActionCable.server.broadcast("activity_feed", {
-      username:      user.username,
-      level:         user.level,
-      activity_type: type,
-      metadata:      metadata,
-      occurred_at:   Time.current
+      type: "moko_party",
+      users: users.map(&:username),
+      message: "#{users.map(&:username).join('と')}のモコたちが集まってパーティを始めました！🎉もこもこ！",
+      occurred_at: Time.current
     })
   end
 end
