1df29b6834cf0abd8abe8c7271208b37034e74ab
furukawa1020 <f.kotaro.0530@gmail.com>
2026-03-27 14:00:26 +0900

    　

--- daigakuos-v2/server/app/controllers/web/dashboard_controller.rb
+++ daigakuos-v2/server/app/controllers/web/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Web::DashboardController < ActionController::Base
   def index
-    @users = User.includes(:sessions, :moko_items).all
+    @users = User.includes(:sessions, :moko_items).all.map do |u|
+      evolution = MokoEvolutionService.check_evolution(u)
+      { user: u, evolution: evolution }
+    end
     @total_users = User.count
     @total_sessions = Session.count
     @total_focus_minutes = Session.sum(:duration) || 0
