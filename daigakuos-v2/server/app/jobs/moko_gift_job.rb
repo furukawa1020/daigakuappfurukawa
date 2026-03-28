da9ed9271540650fc655389795bb399226d6440a
furukawa1020 <f.kotaro.0530@gmail.com>
2026-03-28 15:53:36 +0900

    feat: add MokoGiftJob to automate coin gifting between active users

---
+++ daigakuos-v2/server/app/jobs/moko_gift_job.rb
@@ -0,0 +1,31 @@
+class MokoGiftJob < ApplicationJob
+  queue_as :default
+
+  def perform(sender_id)
+    sender = User.find_by(id: sender_id)
+    return unless sender
+
+    # Find a potential recipient (active in the last 24h)
+    recipient = User.where.not(id: sender.id)
+                    .where('last_sync_at > ?', 24.hours.ago)
+                    .sample
+    
+    return unless recipient
+
+    # Decide on a gift (e.g., 5-20 coins)
+    gift_amount = rand(5..20)
+    recipient.update!(coins: recipient.coins + gift_amount)
+    
+    # Broadcast the news!
+    msg = "#{sender.username}のモコが、#{recipient.username}のモコに#{gift_amount}コインをプレゼントしました！🎁"
+    msg = MokoGrammarService.mokofize(msg)
+    
+    ActionCable.server.broadcast("activity_feed", {
+      type: "moko_gift",
+      message: msg,
+      occurred_at: Time.current
+    })
+    
+    Rails.logger.info "[MokoGift] #{sender.username} sent #{gift_amount} coins to #{recipient.username}"
+  end
+end
