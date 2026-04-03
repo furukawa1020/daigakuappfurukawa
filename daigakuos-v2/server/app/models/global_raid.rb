fc59c2e774d45837c6ee3f853a3fac6dfefe1cc1
furukawa1020 <f.kotaro.0530@gmail.com>
2026-04-03 14:26:16 +0900

    feat: add GlobalRaid model with boss skill mechanics and ActionCable notifications

--- daigakuos-v2/server/app/models/global_raid.rb
+++ daigakuos-v2/server/app/models/global_raid.rb
@@ -9,10 +9,46 @@ class GlobalRaid < ApplicationRecord
 
   after_initialize :set_defaults, if: :new_record?
 
+  BOSS_SKILLS = {
+    'shadow_mist' => { name: "影の霧", effect: "集中効率が半分になるもこ...", duration: 20.minutes },
+    'memory_leak' => { name: "メモリリーク", effect: "ボスのHPが見えなくなるもこ！", duration: 15.minutes },
+    'primal_roar' => { name: "咆哮", effect: "受けるダメージが2倍になるもこ！", duration: 10.minutes }
+  }
+
   def set_defaults
     self.participants_data ||= {}
     self.status ||= 'active'
     self.current_hp ||= self.max_hp
+  end
+
+  def skill_active?
+    active_skill.present? && skill_ends_at.present? && skill_ends_at > Time.current
+  end
+
+  def cast_skill!(skill_id = nil)
+    skill_id ||= BOSS_SKILLS.keys.sample
+    skill = BOSS_SKILLS[skill_id]
+    
+    update!(
+      active_skill: skill_id,
+      skill_ends_at: Time.current + skill[:duration]
+    )
+    
+    # Broadcast to world
+    ActionCable.server.broadcast("raid_channel", {
+      type: "boss_skill_cast",
+      skill_id: skill_id,
+      skill_name: skill[:name],
+      message: "ボスがスキルを放った：#{skill[:name]} ! #{skill[:effect]}"
+    })
+  end
+
+  def clear_skill!
+    update!(active_skill: nil, skill_ends_at: nil)
+    ActionCable.server.broadcast("raid_channel", {
+      type: "skill_cleared",
+      message: "ボスのスキル効果が消えたもこ！"
+    })
   end
 
   def health_percentage
