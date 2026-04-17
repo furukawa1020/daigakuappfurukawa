7641ec0874ec3a04d654c124d922334aa3107c6d
furukawa1020 <f.kotaro.0530@gmail.com>
2026-04-17 18:35:58 +0900

    　

--- daigakuos-v2/ruby_native/core/alchemist.rb
+++ daigakuos-v2/ruby_native/core/alchemist.rb
@@ -5,7 +5,9 @@ class Alchemist
     ['moko_herb', 'moko_honey'] => 'moko_potion',
     ['blueprint_alpha', 'blueprint_beta'] => 'ancient_focus_core',
     ['toxin_residue', 'order_crystal'] => 'neutralizing_shimmer',
-    ['monster_bone', 'iron_ore'] => 'moko_blade_v2'
+    ['monster_bone', 'iron_ore'] => 'moko_blade_v2',
+    ['toxin_residue', 'moko_honey'] => 'probiotic_brew',
+    ['moko_herb', 'monster_bone'] => 'prebiotic_fiber'
   }
 
   def self.combine(item_a, item_b)
